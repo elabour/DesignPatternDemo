15da1c4c74648ff871c2644bee863aa3ffcb8c05
Abdulmottaleb Elabour <aelabour@outlook.com>
2025-05-14 19:21:26 +0200

    Final version

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -10,6 +10,23 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***Singleton Pattern Demo***\n");
+            //Console.WriteLine(Singleton.MyInt);
+            // Private Constructor.So,we cannot use 'new' keyword.
+            Console.WriteLine("Trying to create instance s1.");
+            Singleton s1 = Singleton.GetInstance;
+            Console.WriteLine("Trying to create instance s2.");
+            Singleton s2 = Singleton.GetInstance;
+            if (s1 == s2)
+            {
+                Console.WriteLine("Only one instance exists.");
+            }
+            else
+            {
+                Console.WriteLine("Different instances exist.");
+            }
+            Console.Read();
+
         }
     }
 }
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private int numberOfInstances = 0;
+        // Private constructor to prevent instantiation
+        private Singleton()
+        {
+            Console.WriteLine("Instantiating inside the private constructor.");
+            numberOfInstances++;
+            Console.WriteLine("Number of instances ={0}", numberOfInstances);
+        }
+        // Here is the static constructor
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
