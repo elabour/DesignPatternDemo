8138406b003b20aa0ed25c1ba4e45c872eae024d
Abdulmottaleb Elabour <aelabour@outlook.com>
2025-11-02 18:20:03 +0200

    Adding chain Responsibility

---
+++ ChainOfResponsibilityExample1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityExample1
+{
+    public abstract class SupportHandler
+    {
+        protected SupportHandler nextHandler;
+
+        public void SetNextHandler(SupportHandler handler)
+        {
+            nextHandler = handler;
+        }
+
+        public abstract void HandleRequest(int level);
+    }
+
+    public class Level1Support : SupportHandler
+    {
+        public override void HandleRequest(int level)
+        {
+            if (level == 1)
+            {
+                Console.WriteLine("Level One Support: Issue resolved.");
+            }
+            else if (nextHandler != null)
+            {
+                nextHandler.HandleRequest(level);
+            }
+        }
+    }
+    public class Level2Support : SupportHandler
+    {
+        public override void HandleRequest(int level)
+        {
+            if (level == 2)
+            {
+                Console.WriteLine("Level Two Support: Issue resolved.");
+            }
+            else if (nextHandler != null)
+            {
+                nextHandler.HandleRequest(level);
+            }
+        }
+    }
+
+    public class Manager : SupportHandler
+    {
+        public override void HandleRequest(int level)
+        {
+            if (level == 3)
+            {
+                Console.WriteLine("Manager: Issue resolved.");
+            }
+            else if (nextHandler != null)
+            {
+                nextHandler.HandleRequest(level);
+            }
+            else
+            {
+                Console.WriteLine("No one can handle this request");
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setting up the chain of responsibility
+            SupportHandler level1 = new Level1Support();
+            SupportHandler level2 = new Level2Support();
+            SupportHandler manager = new Manager();
+            level1.SetNextHandler(level2);
+            level2.SetNextHandler(manager);
+            // Simulating support requests
+            Console.WriteLine("Requesting Level 1 Support:");
+            level1.HandleRequest(1);    // Handled by level 1
+            Console.WriteLine("\nRequesting Level 2 Support:");
+            level1.HandleRequest(2); // Escalates to level 2
+            Console.WriteLine("\nRequesting Manager Support:");
+            level1.HandleRequest(3); // Escalates to manager
+            Console.WriteLine("\nRequesting Unknown Level Support:");
+            level1.HandleRequest(4); // No one can handle this request
+        }
+    }
+}
---
+++ ChainOfResponsibilityExample2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityExample2
+{
+    public abstract class Dispenser
+    {
+        protected Dispenser nextDispenser;
+
+        public void SetNext(Dispenser next)
+        {
+            nextDispenser = next;
+        }
+
+        public abstract void Dispense(int amount);
+    }
+
+    public class Dinnars50Dispenser : Dispenser
+    {
+        public override void Dispense(int amount)
+        {
+            if (amount >= 50)
+            {
+                int count = amount / 50;
+                Console.WriteLine("Dispensing " + count + " 50$ note(s)");
+                amount %= 50;
+            }
+            if (nextDispenser != null)
+            {
+                nextDispenser.Dispense(amount);
+            }
+        }
+    }
+    public class Dinnars20Dispenser : Dispenser
+    {
+        public override void Dispense(int amount)
+        {
+            if (amount >= 20)
+            {
+                int count = amount / 20; 
+                Console.WriteLine("Dispensing " + count + " 20$ note(s)");
+                amount %= 20; 
+            }
+            if (nextDispenser != null)
+            {
+                nextDispenser.Dispense(amount);
+            }
+        }
+    }
+    public class Dinnars10Dispenser : Dispenser
+    {
+        public override void Dispense(int amount)
+        {
+            if (amount >= 10)
+            {
+                int count = amount / 10;
+                Console.WriteLine("Dispensing " + count + " 10$ note(s)");
+                amount %= 10;
+            }
+            if (nextDispenser != null)
+            {
+                nextDispenser.Dispense(amount);
+            }
+        }
+    }
+    public class Dinnars5Dispenser : Dispenser
+    {
+        public override void Dispense(int amount)
+        {
+            if (amount >= 5)
+            {
+                int count = amount / 5;
+                Console.WriteLine("Dispensing " + count + " 5$ note(s)");
+                amount %= 5;
+            }
+            if (nextDispenser != null)
+            {
+                nextDispenser.Dispense(amount);
+            }
+        }
+    }
+
+    public class dinar1Dispenser : Dispenser
+    {
+        public override void Dispense(int amount)
+        {
+            if (amount >= 1)
+            {
+                int count = amount / 1;
+                Console.WriteLine("Dispensing " + count + " 1$ note(s)");
+                amount %= 1;
+            }
+            if (nextDispenser != null)
+            {
+                nextDispenser.Dispense(amount);
+            }
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dispenser d50 = new Dinnars50Dispenser();
+            Dispenser d20 = new Dinnars20Dispenser();
+            Dispenser d10 = new Dinnars10Dispenser();
+            Dispenser d5 = new Dinnars5Dispenser();
+            Dispenser d1 = new dinar1Dispenser();
+            d50.SetNext(d20);
+            d20.SetNext(d10);
+            d10.SetNext(d5);
+            d5.SetNext(d1);
+            int amount = 17; // Example amount
+            Console.WriteLine("Dispensing amount: " + amount);
+            d50.Dispense(amount);
+            Console.ReadLine();
+        }
+    }
+}
